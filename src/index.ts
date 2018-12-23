37ab6f2f837e4056ecca403120aebd62011313cb
tett23 <tett23@gmail.com>
2018-12-23 16:47:57 +0900

    Update entry point

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import 'core-js/fn/array/flat-map';
 import { mergeConfig } from './config';
-import { load } from './loader';
+import { build, buildDependencyTable, load } from './loader';
 
-export default function main() {
+export default async function main() {
   const config = mergeConfig({});
-  load(config.targetDirectory, config.loader.extensions);
+  const objects = await load(config.targetDirectory, config.loader.extensions);
+  if (objects == null) {
+    return;
+  }
+
+  const files = build(objects);
+  const dependencies = buildDependencyTable(files);
+
+  console.info(dependencies);
 }
 
 main();
