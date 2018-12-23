929d616a42830b3f836bd1d7ffe57e143910ea32
tett23 <tett23@gmail.com>
2018-12-23 18:29:46 +0900

    Add test

---
+++ __tests__/index.test.ts
@@ -0,0 +1,43 @@
+import { CkusroConfig } from '../src/config';
+import main, { GlobalState } from '../src/index';
+import * as loader from '../src/loader';
+import { mockFileSystem, restoreFileSystem } from './__helpers__/fs';
+
+describe(main.name, () => {
+  beforeEach(() => {
+    mockFileSystem({
+      '/foo/bar/baz.md': '# test file',
+    });
+  });
+  afterEach(() => {
+    restoreFileSystem();
+  });
+
+  function isGlobalState(obj: any): obj is GlobalState {
+    return 'context' in obj && 'files' in obj && 'dependencyTable' in obj;
+  }
+
+  it('returns GlobalState', async () => {
+    const conf: CkusroConfig = {
+      targetDirectory: '/foo',
+      loader: {
+        extensions: /\.(md|txt)$/,
+      },
+    };
+    const actual = await main(conf);
+
+    expect(isGlobalState(actual)).toBeTruthy();
+  });
+
+  it('returns Error when directory does not exist', async () => {
+    const conf: CkusroConfig = {
+      targetDirectory: '/does_not_exist',
+      loader: {
+        extensions: /\.(md|txt)$/,
+      },
+    };
+    const actual = await main(conf);
+
+    expect(actual).toBeInstanceOf(Error);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import 'core-js/fn/array/flat-map';
-import { mergeConfig } from './config';
-import { build, buildDependencyTable, load } from './loader';
+import { CkusroConfig, mergeConfig } from './config';
+import { build, buildDependencyTable, CkusroFile, DependencyTable, load, LoaderContext } from './loader';
 
-export default async function main() {
-  const config = mergeConfig({});
-  const objects = await load(config.targetDirectory, config.loader.extensions);
-  if (objects == null) {
-    return;
+export type GlobalState = {
+  context: LoaderContext;
+  files: CkusroFile[];
+  dependencyTable: DependencyTable;
+};
+
+export default async function main(config: CkusroConfig): Promise<GlobalState | Error> {
+  const result = await load(config.targetDirectory, config.loader.extensions);
+  if (result instanceof Error) {
+    return result;
   }
+  const [context, root] = result;
 
-  const files = build(objects);
+  const files = build(root);
   const dependencies = buildDependencyTable(files);
 
-  console.info(dependencies);
+  return {
+    context,
+    files,
+    dependencyTable: dependencies,
+  };
 }
 
-main();
+const conf = mergeConfig({});
+main(conf);
