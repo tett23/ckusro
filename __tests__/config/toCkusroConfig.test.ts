1e42cea80cafa168161cf4f570be7ab80682a1a4
tett23 <tett23@gmail.com>
2019-01-06 03:39:05 +0900

    Implement toCkusroConfig

---
+++ __tests__/config/toCkusroConfig.test.ts
@@ -0,0 +1,132 @@
+import toCkusroConfig, {
+  isPartializedPrimitiveCkusroConfig,
+  isPartializedPrimitiveLoaderConfig,
+  isRegExpOrString,
+  PrimitiveCkusroConfig,
+  toLoaderConfig,
+  toRegExp,
+} from '../../src/config/toCkusroConfig';
+import { CkusroConfig, LoaderConfig } from '../../src/models/ckusroConfig';
+
+describe(toCkusroConfig, () => {
+  it('converts loaderConfig.extensions', () => {
+    const conf: Partial<PrimitiveCkusroConfig> = {
+      loaderConfig: { extensions: '/test/' },
+    };
+    const actual = toCkusroConfig(conf);
+    const expected: DeepPartial<CkusroConfig> = {
+      loaderConfig: {
+        extensions: /test/,
+      },
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe(isPartializedPrimitiveCkusroConfig, () => {
+  it('returns true when argument is valid object', () => {
+    const data = [{}];
+    data.forEach((value) => {
+      const actual = isPartializedPrimitiveCkusroConfig(value);
+
+      expect(actual).toBe(true);
+    });
+  });
+
+  it('returns false when argument is invalid object', () => {
+    const data = [
+      null,
+      undefined,
+      1,
+      { outputDirectory: 1 },
+      { targetDirectories: 1 },
+    ];
+    data.forEach((value) => {
+      const actual = isPartializedPrimitiveCkusroConfig(value);
+
+      expect(actual).toBe(false);
+    });
+  });
+});
+
+describe(isPartializedPrimitiveLoaderConfig, () => {
+  it('returns true when argument is valid object', () => {
+    const data = [{ extensions: 'foo' }];
+    data.forEach((value) => {
+      const actual = isPartializedPrimitiveLoaderConfig(value);
+
+      expect(actual).toBe(true);
+    });
+  });
+
+  it('returns false when argument is invalid object', () => {
+    const data = [null, undefined, 1, { extensions: null }];
+    data.forEach((value) => {
+      const actual = isPartializedPrimitiveLoaderConfig(value);
+
+      expect(actual).toBe(false);
+    });
+  });
+});
+
+describe(toLoaderConfig, () => {
+  it('converts extensions field', () => {
+    const actual = toLoaderConfig({ extensions: '/foo/' });
+    const expected: DeepPartial<LoaderConfig> = { extensions: /foo/ };
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe(isRegExpOrString, () => {
+  it('returns true when argument is RegExp', () => {
+    const actual = isRegExpOrString(/test/);
+
+    expect(actual).toBe(true);
+  });
+
+  it('returns true when argument is String', () => {
+    const actual = isRegExpOrString('test');
+
+    expect(actual).toBe(true);
+  });
+
+  it('returns false when argument is other type', () => {
+    const data = [1, null, undefined];
+    data.forEach((value) => {
+      const actual = isRegExpOrString(value);
+
+      expect(actual).toBe(false);
+    });
+  });
+});
+
+describe(toRegExp, () => {
+  it('returns RegExp when argument is RegExp', () => {
+    const actual = toRegExp(/foo/);
+
+    expect(actual.source).toBe('foo');
+    expect(actual.flags).toBe('');
+  });
+
+  it('returns RegExp when argument is String', () => {
+    const actual = toRegExp('foo');
+
+    expect(actual.source).toBe('foo');
+    expect(actual.flags).toBe('');
+  });
+
+  it('returns RegExp with flags', () => {
+    const actual = toRegExp('/foo/g');
+
+    expect(actual.source).toBe('foo');
+    expect(actual.flags).toBe('g');
+  });
+
+  it('throw Error when argument is not String', () => {
+    const actual = () => toRegExp(1 as any);
+
+    expect(actual).toThrowError();
+  });
+});
---
+++ src/config/toCkusroConfig.ts
@@ -0,0 +1,130 @@
+import merge from 'lodash.merge';
+import {
+  CkusroConfig,
+  isTargetDirectories,
+  LoaderConfig,
+} from '../models/ckusroConfig';
+import { isNonNullObject } from '../utils/types';
+
+export type PrimitiveLoaderConfig = Omit<LoaderConfig, 'extensions'> & {
+  extensions: string | RegExp;
+};
+export type PrimitiveCkusroConfig = Omit<CkusroConfig, 'loaderConfig'> & {
+  loaderConfig: PrimitiveLoaderConfig;
+};
+
+export function isPartializedPrimitiveCkusroConfig(
+  obj: any,
+): obj is PrimitiveCkusroConfig {
+  if (!isNonNullObject(obj)) {
+    return false;
+  }
+
+  if (!isValidLoaderConfig(obj.loaderConfig)) {
+    return false;
+  }
+
+  if (!isValidTargetDirectories(obj.targetDirectories)) {
+    return false;
+  }
+
+  if (!isValidOutputDirectory(obj.outputDirectory)) {
+    return false;
+  }
+
+  return true;
+}
+
+function isValidOutputDirectory(value: any): boolean {
+  if (value === undefined) {
+    return true;
+  }
+
+  return typeof value === 'string';
+}
+
+function isValidTargetDirectories(value: any): boolean {
+  if (value === undefined) {
+    return true;
+  }
+
+  return isTargetDirectories(value);
+}
+
+function isValidLoaderConfig(value: any): boolean {
+  if (value === undefined) {
+    return true;
+  }
+
+  return isPartializedPrimitiveLoaderConfig(value);
+}
+
+export function isPartializedPrimitiveLoaderConfig(
+  obj: any,
+): obj is DeepPartial<PrimitiveLoaderConfig> {
+  if (!isNonNullObject(obj)) {
+    return false;
+  }
+
+  if (!isValidExtensions(obj.extensions)) {
+    return false;
+  }
+
+  return true;
+}
+
+function isValidExtensions(value: any): boolean {
+  if (value === undefined) {
+    return true;
+  }
+
+  return isRegExpOrString(value);
+}
+
+export default function toCkusroConfig(
+  conf: DeepPartial<PrimitiveCkusroConfig>,
+): DeepPartial<CkusroConfig> {
+  const ret: DeepPartial<CkusroConfig> = merge({}, conf, {
+    loaderConfig: toLoaderConfig(conf.loaderConfig || {}),
+  });
+
+  return ret;
+}
+
+export function toLoaderConfig(
+  conf: DeepPartial<PrimitiveLoaderConfig>,
+): DeepPartial<LoaderConfig> {
+  const { extensions, ...omitExtensions } = conf;
+  const ret: DeepPartial<LoaderConfig> = {
+    ...omitExtensions,
+  };
+
+  if (isRegExpOrString(extensions)) {
+    ret.extensions = toRegExp(extensions);
+  }
+
+  return ret;
+}
+
+export function isRegExpOrString(obj: any): obj is RegExp | string {
+  return typeof obj === 'string' || obj instanceof RegExp;
+}
+
+const regexpPattern = /\/(.+)\/([a-z]?)/;
+
+export function toRegExp(str: string | RegExp): RegExp {
+  if (str instanceof RegExp) {
+    return str;
+  }
+  if (!(typeof str === 'string')) {
+    throw new Error(`Invalid regexp string. str=${str}`);
+  }
+
+  if (!regexpPattern.test(str)) {
+    return new RegExp(str);
+  }
+
+  const [, pattern, flags] = regexpPattern.exec(str) as RegExpExecArray;
+
+  return new RegExp(pattern, flags);
+}
