a079a4d87ade944f79d57005ea6eccfdebe56c34
tett23 <tett23@gmail.com>
2019-01-06 00:17:12 +0900

    Implement isCkusroConfig

--- src/models/ckusroConfig.ts
+++ src/models/ckusroConfig.ts
@@ -13,3 +13,72 @@ export type CkusroConfig = {
   targetDirectories: TargetDirectory[];
   loaderConfig: LoaderConfig;
 };
+
+export function isTargetDirectory(obj: any): obj is TargetDirectory {
+  if (!('path' in obj)) {
+    return false;
+  }
+  if (typeof obj.path !== 'string') {
+    return false;
+  }
+
+  if (!('name' in obj)) {
+    return false;
+  }
+  if (typeof obj.name !== 'string') {
+    return false;
+  }
+
+  if (!('innerPath' in obj)) {
+    return false;
+  }
+  if (typeof obj.innerPath !== 'string') {
+    return false;
+  }
+
+  return true;
+}
+
+export function isTargetDirectories(obj: any): obj is TargetDirectory[] {
+  if (!Array.isArray(obj)) {
+    return false;
+  }
+
+  return obj.every(isTargetDirectory);
+}
+
+export function isLoaderConfig(obj: any): obj is LoaderConfig {
+  if (!('extensions' in obj)) {
+    return false;
+  }
+  if (!(obj.extensions instanceof RegExp)) {
+    return false;
+  }
+
+  return true;
+}
+
+export function isCkusroConfig(obj: any): obj is CkusroConfig {
+  if (!('outputDirectory' in obj)) {
+    return false;
+  }
+  if (typeof obj.outputDirectory !== 'string') {
+    return false;
+  }
+
+  if (!('targetDirectories' in obj)) {
+    return false;
+  }
+  if (!isTargetDirectories(obj.targetDirectories)) {
+    return false;
+  }
+
+  if (!('loaderConfig' in obj)) {
+    return false;
+  }
+  if (!isLoaderConfig(obj.loaderConfig)) {
+    return false;
+  }
+
+  return true;
+}
