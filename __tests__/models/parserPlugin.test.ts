eb0e6dcef6f99968f7c826a06deec0cdeae531ab
tett23 <tett23@gmail.com>
2019-01-06 20:41:23 +0900

    Add ParserPlugin type

---
+++ __tests__/models/parserPlugin.test.ts
@@ -0,0 +1,41 @@
+import { isParserPlugin, isParserPlugins } from '../../src/models/parserPlugin';
+
+describe(isParserPlugin, () => {
+  it('judges type', () => {
+    const data: Array<[any, boolean]> = [
+      [{ name: 'test', plugin: () => {} }, true], // tslint:disable-line no-empty
+      [{}, false],
+      [null, false],
+      [undefined, false],
+      [1, false],
+    ];
+    data.forEach(([value, expected]) => {
+      const actual = isParserPlugin(value);
+
+      expect(actual).toBe(expected);
+    });
+  });
+});
+
+describe(isParserPlugins, () => {
+  it('judges type', () => {
+    const data: Array<[any, boolean]> = [
+      [[], true],
+      [[{ name: 'test', plugin: () => {} }], true], // tslint:disable-line no-empty
+      [{ name: 'test', plugin: () => {} }, false], // tslint:disable-line no-empty
+      [{}, false],
+      [null, false],
+      [undefined, false],
+      [1, false],
+      [[{}], false],
+      [[null], false],
+      [[undefined], false],
+      [[1], false],
+    ];
+    data.forEach(([value, expected]) => {
+      const actual = isParserPlugins(value);
+
+      expect(actual).toBe(expected);
+    });
+  });
+});
