b294e81ed03800f121e78553ed8a5caf802dd8c3
tett23 <tett23@gmail.com>
2019-01-06 21:44:39 +0900

    Remove unused variable

--- __tests__/staticRenderer/render.test.ts
+++ __tests__/staticRenderer/render.test.ts
@@ -1,14 +1,9 @@
 import render from '../../src/staticRenderer/render';
-import {
-  buildFile,
-  buildGlobalState,
-  buildLoaderContext,
-} from '../__fixtures__';
+import { buildFile, buildGlobalState } from '../__fixtures__';
 
 describe(render, () => {
   it('renders correctly', () => {
     const file = buildFile();
-    const contexts = [buildLoaderContext({ name: file.namespace })];
     const globalState = buildGlobalState({ files: [file] });
     const actual = render({
       globalState,
