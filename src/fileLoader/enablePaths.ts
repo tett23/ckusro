98141a2e86f9bfdf5bc4c40051bae5780be59aa6
tett23 <tett23@gmail.com>
2019-01-20 09:45:26 +0900

    Extract logic

--- src/fileLoader/enablePaths.ts
+++ src/fileLoader/enablePaths.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 import { LoaderConfig } from '../models/ckusroConfig/LoaderConfig';
 import { LoaderContext } from '../models/loaderContext';
 
-export async function enablePaths(
+export default async function enablePaths(
   loaderContext: LoaderContext,
   loaderConfig: LoaderConfig,
 ): Promise<Array<[LoaderContext, string]> | Error> {
@@ -15,8 +15,16 @@ export async function enablePaths(
     return paths;
   }
 
+  const filtered = filterPaths(paths, loaderConfig);
+
+  return filtered.map((item): [LoaderContext, string] => [loaderContext, item]);
+}
+
+export function filterPaths(
+  paths: string[],
+  loaderConfig: LoaderConfig,
+): string[] {
   return paths
     .filter((item) => [loaderConfig.enable].some((re) => re.test(item)))
-    .filter((item) => loaderConfig.ignore.every((re) => !re.test(item)))
-    .map((item): [LoaderContext, string] => [loaderContext, item]);
+    .filter((item) => loaderConfig.ignore.every((re) => !re.test(item)));
 }
