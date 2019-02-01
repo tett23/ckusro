16677e1367f72a6ef1ce014b910f00b13fd97f11
tett23 <tett23@gmail.com>
2019-02-01 20:45:08 +0900

    Add loadContents tests

--- __tests__/core/LoaderInfoBuilder/NodeFS/loadContents.test.ts
+++ __tests__/core/LoaderInfoBuilder/NodeFS/loadContents.test.ts
@@ -2,13 +2,57 @@ import { FileInfo } from '../../../../src/core/LoaderInfoBuilder/NodeFS/fetchEnt
 import loadContents, {
   loadEachItem,
 } from '../../../../src/core/LoaderInfoBuilder/NodeFS/loadContents';
-import { FileModeFile } from '../../../../src/models/StatType';
+import {
+  FileModeDirectory,
+  FileModeFile,
+} from '../../../../src/models/StatType';
+import { UnloadedFile } from '../../../../src/models/UnloadedFile';
 import {
   buildLocalLoaderContext,
   buildUnloadedFile,
 } from '../../../__fixtures__';
 
-describe(loadContents, () => {});
+describe(loadContents, () => {
+  it('returns UnloadedFile-string tuples', async () => {
+    const readFile = jest.fn().mockResolvedValueOnce('# test Markdown');
+    const loaderContext = buildLocalLoaderContext();
+    const entries: FileInfo[] = [
+      ['/test.md', FileModeFile],
+      ['/test', FileModeDirectory],
+    ];
+    const actual = await loadContents(readFile, loaderContext, entries);
+    const expected: Array<[UnloadedFile, string | Buffer | null]> | Error[] = [
+      [
+        buildUnloadedFile({
+          loaderContext,
+          absolutePath: '/test.md',
+          mode: FileModeFile,
+        }),
+        '# test Markdown',
+      ],
+      [
+        buildUnloadedFile({
+          loaderContext,
+          absolutePath: '/test',
+          mode: FileModeDirectory,
+        }),
+        null,
+      ],
+    ];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('returns Errro[] when loadEachItem threw Error', async () => {
+    const err = new Error();
+    const readFile = jest.fn().mockRejectedValue(err);
+    const loaderContext = buildLocalLoaderContext();
+    const entries: FileInfo[] = [['/test.md', FileModeFile]];
+    const actual = await loadContents(readFile, loaderContext, entries);
+
+    expect(actual).toEqual([err]);
+  });
+});
 
 describe(loadEachItem, async () => {
   it('returns UnloadedFile-string tuple', async () => {
