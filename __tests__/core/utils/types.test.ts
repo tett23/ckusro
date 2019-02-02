7d89a7a2b8c5746e93a65878e32bd39aba11764d
tett23 <tett23@gmail.com>
2019-02-02 13:36:25 +0900

    Implement type utilities

--- __tests__/core/utils/types.test.ts
+++ __tests__/core/utils/types.test.ts
@@ -1,48 +1,171 @@
-import { isErrors, isNonNullObject } from '../../../src/core/utils/types';
+import {
+  hasProperty,
+  isArrayOf,
+  isErrors,
+  isNonNullObject,
+  isPropertyTypeOf,
+  isPropertyValidTypeOf,
+  isTypeOf,
+  isValidTypeOf,
+} from '../../../src/core/utils/types';
 
 describe(isNonNullObject, () => {
   it('judges types', () => {
-    const data: Array<[any, boolean]> = [
-      [{}, true],
-      [{ a: 1 }, true],
-      [null, false],
-      [[], false],
-      [undefined, false],
-      [true, false],
-      [1, false],
-      [() => {}, false], // tslint:disable-line no-empty
-    ];
-
-    data.forEach(([value, expected]) => {
-      const actual = isNonNullObject(value);
-
-      expect(actual).toBe(expected);
-    });
+    validate(isNonNullObject, [
+      [[{}], true],
+      [[{ a: 1 }], true],
+      [[null], false],
+      [[[]], false],
+      [[undefined], false],
+      [[true], false],
+      [[1], false],
+      [[() => {}], false], // tslint:disable-line no-empty
+    ]);
   });
 });
 
 describe(isErrors, () => {
   it('judges types', () => {
-    const data: Array<[any, boolean]> = [
-      [[new Error()], true],
-      [[new Error(), new Error()], true],
-      [[], false],
-      [[new Error(), 1], false],
-      [[1, 1], false],
-      [new Error(), false],
-      [undefined, false],
-      [null, false],
-      [true, false],
-      [1, false],
-      [[], false],
-      [{}, false],
-      [() => {}, false], // tslint:disable-line no-empty
-    ];
+    validate(isErrors, [
+      [[[new Error()]], true],
+      [[[new Error(), new Error()]], true],
+      [[[]], false],
+      [[[new Error(), 1]], false],
+      [[[1, 1]], false],
+      [[new Error()], false],
+      [[undefined], false],
+      [[null], false],
+      [[true], false],
+      [[1], false],
+      [[[]], false],
+      [[{}], false],
+      [[() => {}], false], // tslint:disable-line no-empty
+    ]);
+  });
+});
 
-    data.forEach(([value, expected]) => {
-      const actual = isErrors(value);
+describe(isArrayOf, () => {
+  it('judges types', () => {
+    const validator = (obj: unknown): obj is number => typeof obj === 'number';
 
-      expect(actual).toBe(expected);
-    });
+    validate(isArrayOf, [
+      [[[], validator], true],
+      [[[1], validator], true],
+      [[[1, true], validator], false],
+      [[undefined, validator], false],
+      [[null, validator], false],
+      [[true, validator], false],
+      [[1, validator], false],
+      [[{}, validator], false],
+      [[() => {}, validator], false], // tslint:disable-line no-empty
+    ]);
+  });
+});
+
+type ValidateData = Array<[any[], boolean]>;
+function validate(func: (...args: any[]) => boolean, data: ValidateData) {
+  data.forEach(([args, expected]) => {
+    const actual = func(...args);
+
+    expect(actual).toBe(expected);
+  });
+}
+
+describe(isPropertyValidTypeOf, () => {
+  it('judges types', () => {
+    const validator = (obj: unknown): obj is number => typeof obj === 'number';
+
+    validate(isPropertyValidTypeOf, [
+      [[{ exists: 1 }, 'exists', validator], true],
+      [[{ exists: true }, 'exists', validator], false],
+      [[{ exists: undefined }, 'exists', validator], false],
+      [[{ exists: 1 }, 'does_not_exists', validator], false],
+      [[{}, 'does_not_exists', validator], false],
+      [[1, 'does_not_exists', validator], false],
+      [[true, 'does_not_exists', validator], false],
+      [[undefined, 'does_not_exists', validator], false],
+      [[null, 'does_not_exists', validator], false],
+      [[() => {}, 'does_not_exists', validator], false], // tslint:disable-line no-empty
+    ]);
+  });
+});
+
+describe(isValidTypeOf, () => {
+  it('judges types', () => {
+    const validator = (obj: unknown): obj is number => typeof obj === 'number';
+
+    validate(isValidTypeOf, [
+      [[1, validator], true],
+      [[{}, validator], false],
+      [[true, validator], false],
+      [[undefined, validator], false],
+      [[null, validator], false],
+      [[() => {}, validator], false], // tslint:disable-line no-empty
+    ]);
+  });
+});
+
+describe(isPropertyTypeOf, () => {
+  it('judges types', () => {
+    validate(isPropertyTypeOf, [
+      [[{ exists: 1 }, 'exists', 'number'], true],
+      [[{ exists: true }, 'exists', 'number'], false],
+      [[{ exists: undefined }, 'exists', 'number'], false],
+      [[{ exists: 1 }, 'does_not_exists', 'number'], false],
+      [[{}, 'does_not_exists', 'number'], false],
+      [[1, 'does_not_exists', 'number'], false],
+      [[true, 'does_not_exists', 'number'], false],
+      [[undefined, 'does_not_exists', 'number'], false],
+      [[null, 'does_not_exists', 'number'], false],
+      [[() => {}, 'does_not_exists', 'number'], false], // tslint:disable-line no-empty
+    ]);
+  });
+});
+
+describe(isTypeOf, () => {
+  it('judges types', () => {
+    validate(isTypeOf, [
+      [['', 'string'], true],
+      [[true, 'string'], false],
+      [[1, 'number'], true],
+      [[true, 'number'], false],
+      [[true, 'boolean'], true],
+      [[false, 'boolean'], true],
+      [[1, 'boolean'], false],
+      [[null, 'null'], true],
+      [[undefined, 'null'], false],
+      [[true, 'null'], false],
+      [[undefined, 'undefined'], true],
+      [[null, 'undefined'], false],
+      [[[], 'array'], true],
+      [[[1], 'array'], true],
+      [[[1, true], 'array'], true],
+      [[{}, 'array'], false],
+      [[1, 'array'], false],
+      [[null, 'array'], false],
+      [[{}, 'object'], true],
+      [[{ a: 1 }, 'object'], true],
+      [[null, 'object'], false],
+      [[true, 'object'], false],
+      [[() => {}, 'function'], true], // tslint:disable-line no-empty
+      [[null, 'function'], false],
+      [[undefined, 'function'], false],
+      [[true, 'function'], false],
+    ]);
+  });
+});
+
+describe(hasProperty, () => {
+  it('judges types', () => {
+    validate(hasProperty, [
+      [[{ exists: 1 }, 'exists', 'number'], true],
+      [[{ exists: 1 }, 'does_not_exists', 'number'], false],
+      [[{}, 'does_not_exists', 'number'], false],
+      [[1, 'does_not_exists', 'number'], false],
+      [[true, 'does_not_exists', 'number'], false],
+      [[undefined, 'does_not_exists', 'number'], false],
+      [[null, 'does_not_exists', 'number'], false],
+      [[() => {}, 'does_not_exists', 'number'], false], // tslint:disable-line no-empty
+    ]);
   });
 });
--- src/core/utils/types.ts
+++ src/core/utils/types.ts
@@ -20,5 +20,106 @@ export function isErrors(obj: unknown): obj is Error[] {
     return false;
   }
 
-  return obj.every((item) => item instanceof Error);
+  return true;
+}
+
+export type TypeNames =
+  | 'string'
+  | 'number'
+  | 'boolean'
+  | 'null'
+  | 'undefined'
+  | 'array'
+  | 'object'
+  | 'function';
+
+export type Validator<T> = (value: unknown) => value is T;
+
+export type AnyObject = {
+  [key: string]: AnyValue;
+};
+export type AnyArray = any[];
+
+export type AnyValue =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | Function // tslint:disable-line ban-types
+  | AnyArray
+  | AnyObject;
+
+export function isArrayOf<T>(
+  obj: unknown,
+  validator: Validator<T>,
+): obj is T[] {
+  if (!isTypeOf<T[]>(obj, 'array')) {
+    return false;
+  }
+
+  return obj.every((item) => validator(item));
+}
+
+export function isPropertyValidTypeOf<O extends object, P extends string, T>(
+  obj: O,
+  property: P,
+  validator: Validator<T>,
+): obj is O & Record<P, T> {
+  if (!hasProperty(obj, property)) {
+    return false;
+  }
+
+  return isValidTypeOf(obj[property], validator);
+}
+
+export function isValidTypeOf<T>(
+  value: unknown,
+  validator: (arg: unknown) => arg is T,
+): value is T {
+  return validator(value);
+}
+
+export function isPropertyTypeOf<O extends object, P extends string, T>(
+  obj: O,
+  property: P,
+  type: TypeNames,
+): obj is O & Record<P, T> {
+  if (!hasProperty(obj, property)) {
+    return false;
+  }
+
+  return isTypeOf(obj[property], type);
+}
+
+export function isTypeOf<T>(value: unknown, type: TypeNames): value is T {
+  switch (type) {
+    case 'string':
+      return typeof value === 'string';
+    case 'number':
+      return typeof value === 'number';
+    case 'boolean':
+      return typeof value === 'boolean';
+    case 'null':
+      return value === null;
+    case 'undefined':
+      return typeof value === 'undefined';
+    case 'array':
+      return Array.isArray(value);
+    case 'object':
+      return isNonNullObject(value);
+    case 'function':
+      return typeof value === 'function';
+  }
+}
+
+export function hasProperty<O extends object, P extends string>(
+  obj: O,
+  property: P,
+): obj is O & Record<P, AnyValue> {
+  if (!isNonNullObject(obj)) {
+    return false;
+  }
+
+  return obj.hasOwnProperty(property);
 }
